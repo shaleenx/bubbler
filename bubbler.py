7e8dfb6ef5436ba77dfa8e9bded909f2921111ac
shaleenx <shaleenx@gmail.com>
2016-10-05 20:40:43 +0530

    Add Scorer and display results

--- bubbler.py
+++ bubbler.py
@@ -4,7 +4,7 @@
 import numpy as np
 import cv2
 import argparse
-
+from imutils import contours
 
 def order_points(pts):
     '''initialize a list of coordinates that will be ordered such that they are in the order
@@ -70,20 +70,20 @@ im_edged = cv2.Canny(blurred, 75, 200)  # find edges
 
 # find contours in the edge map and initialize the contour
 # that corresponds to the document
-contours = cv2.findContours(im_edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours = contours[0] if imutils.is_cv2() else contours[1]
+cnts = cv2.findContours(im_edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 docCnt = None
 
 # ensure that atleast one contour was found
-if len(contours) > 0:
+if len(cnts) > 0:
     # sort the contours according to their size in descending order
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
     # loop over the sorted contours
-    for c in contours:
+    for c in cnts:
         # approximate the contour
         peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02*peri, True)
+        approx = cv2.roxPolyDP(c, 0.02*peri, True)
 
         # if our approximated contour has four points,
         # then we can assume we have found the paper
@@ -101,12 +101,12 @@ thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[
 
 # find contours in the thresholded image, then initialize the list of contours that
 # correspond to questions
-contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours = contours[0] if imutils.is_cv2() else contours[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 questionCnts = []
 
 # loop over the contours
-for c in contours:
+for c in cnts:
     # compute the bounding box of the contour, then use the bounding box
     # to derive the aspect ratio
     (x, y, w, h) = cv2.boundingRect(c)
@@ -116,4 +116,52 @@ for c in contours:
     # and sufficiently tall, and have an aspect ratio of almost 1
     if w>=20 and h>=20 and ar>=0.9 and ar<1.1:
         questionCnts.append(c)
+
+# sort the question contours top-to-bottom, then initialize the total number of correct answers
+questionCnts = contours.sort_contours(questionCnts, method="top-to-bottom")[0]
+correct = 0
+
+# each question has 5 possible answers, to loop over the question in batches of 5
+for (q, i) in enumerate(np.arrange(0, len(questionCnts), 5)):
+    # sort the contours for the current question from left to right, then
+    # initialize the index of the bubbled answer
+    cnts = contours.sort_contours(questionCnts[i:i+5])[0]
+    bubbled = None
+
+    # loop over the sorted contours
+    for (j,c) in enumerate(cnts):
+        # construct a mask that reveals only the current "bubble" for the question
+        mask = np.zeros(thresh.shape, dtype="uint8")
+        cv2.drawContours(mask, [c], -1, 255, -1)
+
+        # apply the mask to the threshold image, then count the number of non-zero
+        # pixels in the bubble area
+        mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+        total = cv2.countNonZero(mask)
+
+        # if the current total has a larger number of total non-zero pixels, then
+        # we are examining the currently bubbled-in answer
+        if bubbled is None or total>bubbled[0]:
+            bubbled = (total, j)
+
+    # initialize the contour color and the index of the "correct" answer
+    color = (0, 0, 255)
+    k = ANSWER_KEY[q]
+
+    # check to see if the bubbled answer is correct actually
+    if k == bubbled[1]:
+        color = (0, 255, 0)
+        correct += 1
+
+    # draw the outline of the correct answer on the test
+    cv2.drawContours(paper, [cnts[k]], -1, color, 3)
+
+# scoring the exam and displaying the results to the screen
+score = (correct/5.0)*100
+print("Score: {:.2f}%".format(score))
+cv2.putText(paper, "{:.2f}%".format(score), (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+        0.9, (0, 0, 255), 2)
+cv2.imshow("Original", image)
+cv2.imshow("Exam", paper)
+cv2.waitKey(0)
 
